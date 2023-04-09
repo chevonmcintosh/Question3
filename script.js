34b45aa35f22bda3ece1fc374bfd6eab1789397e
chevonmcintosh <92833168+chevonmcintosh@users.noreply.github.com>
2023-04-09 18:36:37 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+"use strick";
+
+$(document).ready(function () {
+  $("#example").DataTable({
+    ajax: "data/objects.txt",
+    columns: [
+      { data: "name" },
+      { data: "position" },
+      { data: "office" },
+      { data: "extn" },
+      { data: "start_date" },
+      { data: "salary" },
+    ],
+  });
+});
